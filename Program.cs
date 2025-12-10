8a9f3c30dd124dbcd38b606733b556760dcd6fab
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-10 15:15:08 +0500

    Создание метода GetContent. Mamaev

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PR7
 {
@@ -13,34 +10,42 @@ namespace PR7
     {
         static void Main(string[] args)
         {
-            SignIn("user", "user");
-            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            reader.Close();
+            Cookie token = SignIn("user", "user");
+            GetContent(token);
             Console.Read();
         }
-        public static void SignIn(string Login, string Password)
+        public static Cookie SignIn(string Login, string Password)
         {
+            Cookie token = null;
             string url = "http://news.permaviat.ru/ajax/login.php";
-            Debug.WriteLine($"Выполняем запрос:{url}");
+            Debug.WriteLine($"Выполняю запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "POST";
+            request.Method = "Post";
             request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = new CookieContainer();
             string postData = $"login={Login}&password={Password}";
             byte[] Data = Encoding.ASCII.GetBytes(postData);
             request.ContentLength = Data.Length;
-            using (var stream = request.GetRequestStream())
+            using (Stream stream = request.GetRequestStream())
             {
                 stream.Write(Data, 0, Data.Length);
             }
+            using (HttpWebResponse Response = (HttpWebResponse)request.GetResponse())
+            {
+                Debug.WriteLine($"Статус выполенения: {Response.StatusCode}");
+                string ResponseFromServer = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(ResponseFromServer);
+                token = Response.Cookies["token"];
+            }
+            return token;
+        }
+        public static void GetContent(Cookie Token)
+        {
+            string url = "http://news.permaviat.ru/main";
+            Debug.WriteLine($"Выполняем зпарос: {url}");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = new CookieContainer();
+            request.CookieContainer.Add(Token);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
             string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
